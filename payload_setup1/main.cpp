7f00b65a52ef1316b9cc21c4df0229bcc1cfd467
gengwei <gengw16@mails.tsinghua.edu.cn>
2020-01-06 03:55:13 +0800

    初步实现setup部分的功能

--- payload_setup1/main.cpp
+++ payload_setup1/main.cpp
@@ -1,15 +1,43 @@
-//
-//  main.cpp
-//  payload_setup1
-//
-//  Created by 耿威 on 2020/1/6.
-//  Copyright © 2020 耿威. All rights reserved.
-//
-
 #include <iostream>
+#include <string.h>
+#include "libverimb/pattern_loader.h"
+#include "libverimb/ac/ac_adaptor.h"
+
+using namespace std;
+
+PatternSet patterns;
+ACAdaptor* engine;
+
+
+string pattern_path = "/Users/gengwei/Desktop/veriMB/payload_setup1/payload_setup1/snort_test.txt";
+
+void print_patterns(PatternSet& ptns)
+{
+    for(int i=0; i < ptns.size(); i++)
+    {
+        for(int j = 0; j < ptns[i].size(); j++)
+        {
+            cout<<(char)ptns[i][j];
+        }
+        cout<<endl;
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    /*读入处理好后的pattern文件*/
+    PatternLoader::load_pattern_file(pattern_path.data(), patterns);
+    //print_patterns(patterns);
+    
+    /*保存规则集id、规则数目meta和secret*/
+    /*生成secret*/
+    
+    /*建AC树，AC树的结构保存在engine.ac中*/
+    engine = new ACAdaptor();
+    engine->init(patterns);
+    
+    /*计算每个节点的HMAC和每条规则的HMAC*/
+    
+    
     return 0;
 }
