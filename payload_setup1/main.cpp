b3d23537a32060fcd147b877962c230d0efbe52c
gengwei <gengw16@mails.tsinghua.edu.cn>
2020-01-06 00:37:01 +0800

    Initial Commit

---
+++ payload_setup1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  payload_setup1
+//
+//  Created by 耿威 on 2020/1/6.
+//  Copyright © 2020 耿威. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
